d8312375fe7bde5538bb32effdb95060fa08493b
linnykerxd9 <linnykerxd9@gmail.com>
2021-06-22 21:50:12 -0300

    criando um hook simples para a autenticação

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,9 @@
+import { useContext } from 'react'
+import { authContext } from '../contexts/AuthContexts'
+
+
+export function useAuth() {
+    const value = useContext(authContext);
+
+    return value;
+}
