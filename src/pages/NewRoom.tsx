ff20230a816736617606a803999d5b7881308428
linnykerxd9 <linnykerxd9@gmail.com>
2021-06-25 00:25:16 -0300

    redirecionando o adm para a page de adm da sala

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -27,7 +27,7 @@ async function handleCreateRoom(event: FormEvent) {
         authorId: user?.id,
     })
     
-        history.push(`/rooms/${firebaseRoom.key}`)
+        history.push(`/admin/rooms/${firebaseRoom.key}`)
     }
     
     return (
